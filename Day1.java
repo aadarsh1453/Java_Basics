76146db60f7dd6536317434c54bca15dcd9516ac
Aadarshkumar Sharma <113667076+aadarsh1453@users.noreply.github.com>
2024-04-01 10:47:47 +0530

    Day1.java

--- Day1.java
+++ Day1.java
@@ -1,48 +1,48 @@
-
+// 
 import java.util.*;
-// public class Day1 { 
+ public class Day1 { 
 
-//     public static void main(String[] args)     //in c++ commandline argument always take 1 first argument as file name but not in java.
-//     {
-//                         /// It required to run this program only through command line otherwise give an error because 
-//                         /// java not take filename location as argument like C++.
-//         System.out.println(args[0]);
-//         System.out.println(args.length);
+     public static void main(String[] args)     //in c++ commandline argument always take 1 first argument as file name but not in java.
+     {
+                         /// It required to run this program only through command line otherwise give an error because 
+                         /// java not take filename location as argument like C++.
+         System.out.println(args[0]);
+         System.out.println(args.length);
         
-//     }
+     }
     
-// }
+ }
 
 
 
 
-// public class Day1
-// {
-//     public static void main(String[] args)
-//     {
-//         System.out.print("Hello in Java Program.");
+ public class Day1
+ {
+     public static void main(String[] args)
+     {
+         System.out.print("Hello in Java Program.");
 
-//     }
-// }
-
-
+     }
+ }
 
 
-// public class Day1
-// {
-//     public static void main(String[] args)
-//     {
-//         String str, str1;
-//         Scanner scanner = new Scanner(System.in);
-//         str = scanner.nextLine();
-//         str1= scanner.nextLine();
 
 
-//         if(str1 == str)
-//             System.out.println("Both string are equals");
-//         else System.out.println("Strings are not equals.");
-//     }
-// }
+ public class Day1
+ {
+     public static void main(String[] args)
+     {
+         String str, str1;
+         Scanner scanner = new Scanner(System.in);
+         str = scanner.nextLine();
+         str1= scanner.nextLine();
+
+
+         if(str1 == str)
+             System.out.println("Both string are equals");
+         else System.out.println("Strings are not equals.");
+     }
+ }
 
 
 
